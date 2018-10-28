f99742f7c5ded048e8449bec700456e825bd6ade
Yakov Gazizov <imcuber@live.ru>
2018-10-28 18:17:00 +0300

    Add HLEB_FOREACH macro

--- variadic_utils.h
+++ variadic_utils.h
@@ -10,5 +10,41 @@
 #define HLEB_COUNT_ARGS(...)                                                \
     $HLEB_COUNT_ARGS(, ##__VA_ARGS__, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0)
 
+#define $HLEB_DO_SELECTED(count, pref, args...)                             \
+    $$HLEB_DO_SELECTED(count, pref, args)
+#define $$HLEB_DO_SELECTED(count, pref, args...) pref##_##count(args)
+
+#define HLEB_DO_SELECTED(pref, args...)                                     \
+    $HLEB_DO_SELECTED(HLEB_COUNT_ARGS(args), pref, args)
+
+#define HLEB_FIRST_ARG(f, o...)     f
+#define HLEB_OTHER_ARGS(f, o...)    o
+
+#define $HLEB_FOREACH_1(macro, args...) macro(args)
+#define $HLEB_FOREACH_2(macro, args...)                                     \
+    macro(HLEB_FIRST_ARG(args)) $HLEB_FOREACH_1(macro, HLEB_OTHER_ARGS(args))
+#define $HLEB_FOREACH_3(macro, args...)                                     \
+    macro(HLEB_FIRST_ARG(args)) $HLEB_FOREACH_2(macro, HLEB_OTHER_ARGS(args))
+#define $HLEB_FOREACH_4(macro, args...)                                     \
+    macro(HLEB_FIRST_ARG(args)) $HLEB_FOREACH_3(macro, HLEB_OTHER_ARGS(args))
+#define $HLEB_FOREACH_5(macro, args...)                                     \
+    macro(HLEB_FIRST_ARG(args)) $HLEB_FOREACH_4(macro, HLEB_OTHER_ARGS(args))
+#define $HLEB_FOREACH_6(macro, args...)                                     \
+    macro(HLEB_FIRST_ARG(args)) $HLEB_FOREACH_5(macro, HLEB_OTHER_ARGS(args))
+#define $HLEB_FOREACH_7(macro, args...)                                     \
+    macro(HLEB_FIRST_ARG(args)) $HLEB_FOREACH_6(macro, HLEB_OTHER_ARGS(args))
+#define $HLEB_FOREACH_8(macro, args...)                                     \
+    macro(HLEB_FIRST_ARG(args)) $HLEB_FOREACH_7(macro, HLEB_OTHER_ARGS(args))
+#define $HLEB_FOREACH_9(macro, args...)                                     \
+    macro(HLEB_FIRST_ARG(args)) $HLEB_FOREACH_8(macro, HLEB_OTHER_ARGS(args))
+#define $HLEB_FOREACH_10(macro, args...)                                    \
+    macro(HLEB_FIRST_ARG(args)) $HLEB_FOREACH_9(macro, HLEB_OTHER_ARGS(args))
+
+
+#define $HLEB_FOREACH(macro, args...)                                       \
+    $HLEB_DO_SELECTED(HLEB_COUNT_ARGS(args), $HLEB_FOREACH, macro, args)
+#define HLEB_FOREACH(macro, args...)                                        \
+    $HLEB_FOREACH(macro, args)
+
 #endif // HLEB_VARIADIC_UTILS_H
 
